d8b93a08e48d0e10e66c55420c5e57935b36206b
nhd <135978438+nhdfr@users.noreply.github.com>
2025-10-19 14:18:04 +0530

    feat: add the code buffs, and loacal syncing (#12)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,5 @@
-import HeroSection from "@/components/Hero";
-import IconSection from "@/components/IconSection";
-import Navbar from "@/components/Navbar";
+import { redirect } from "next/navigation";
 
-const page = () => {
-	return (
-		<div className="from-bgDark to-bgDark relative min-h-screen overflow-hidden bg-gradient-to-br via-zinc-900">
-			<Navbar />
-			<HeroSection />
-			<div className="relative z-100 mx-auto max-w-7xl px-6 lg:px-8">
-				<IconSection />
-			</div>
-		</div>
-	);
-};
-
-export default page;
+export default function Page() {
+  redirect("/editor");
+}
